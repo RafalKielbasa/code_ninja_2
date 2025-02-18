e76a0827b15835e12883d91693f6c93a75bec2b8
LukSt92 <lukasz.m.stasiak@gmail.com>
2025-02-18 22:33:37 +0100

    ,

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
-import './App.css'
-import { useState } from 'react'
-import { WishForm } from './components/WishForm'
-import { WishList } from './components/WishList'
+import "./App.css";
+import { useState } from "react";
+import { WishForm } from "./components/WishForm";
+import { WishList } from "./components/WishList";
+import styled from "styled-components";
 
+const StyledApp = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+`;
 
 function App() {
-  const [giftsList,setGiftList] =useState([]) 
- 
-  return(
-  <>
-  <WishList gifts={giftsList}
-  />
-<WishForm setGiftList={setGiftList}/>
-  </>)
+  const [giftsList, setGiftList] = useState([]);
+
+  console.log(giftsList);
+
+  function removeGift(id) {
+    setGiftList((prev) => prev.filter((gift) => gift.id != id));
+  }
+
+  return (
+    <StyledApp>
+      <WishList gifts={giftsList} removeGift={removeGift} />
+      <WishForm setGiftList={setGiftList} />
+    </StyledApp>
+  );
 }
 
-
-export default App
+export default App;
--- src/components/WishList.jsx
+++ src/components/WishList.jsx
@@ -1,35 +1,41 @@
-import Table from '@mui/material/Table'
-import TableBody from '@mui/material/TableBody'
-import TableCell from '@mui/material/TableCell'
-import TableHead from '@mui/material/TableHead'
-import TableRow from '@mui/material/TableRow'
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import { GiftsTableRow } from "./GiftsTableRow";
+import styled from "styled-components";
 
-export const WishList = ({ gifts = [] }) => {
+const StyledWishList = styled.div`
+  display: flex;
+  flex: 1;
+  padding: 4px;
+`;
+
+export const WishList = ({ gifts = [], removeGift }) => {
   return (
-    <div>
+    <StyledWishList>
       <Table>
         <TableHead>
           <TableRow>
             <TableCell>Gift</TableCell>
             <TableCell>Person</TableCell>
             <TableCell>Price</TableCell>
+            <TableCell>Priority</TableCell>
+            <TableCell>Actions</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {gifts?.map((gift, index) => {
-            const giftArray = Object.entries(gift)
-              .filter(([key]) => key !== 'id')
-              .map(([_, value]) => value)
-            return (
-              <TableRow key={gift.id}>
-                {giftArray?.map((item) => (
-                  <TableCell key={`${item}-${index}`}>{item}</TableCell>
-                ))}
-              </TableRow>
-            )
-          })}
+          {gifts?.map((gift, index) => (
+            <GiftsTableRow
+              key={index}
+              gift={gift}
+              giftIndex={index}
+              removeGift={removeGift}
+            />
+          ))}
         </TableBody>
       </Table>
-    </div>
-  )
-}
+    </StyledWishList>
+  );
+};
